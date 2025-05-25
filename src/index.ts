7e691ac2ce1d2d00113ac5d5bc9796ac93ec4398
Jacob Nichols <jacobnichols@form.io>
2025-05-24 20:11:24 -0500

    create jnvm list available command

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-const command = process.argv[2];
+import {table} from 'console-table-without-index';
+import {getAvailableVersions} from "./commands/list";
 
-switch (command) {
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(arg2);
+switch (arg1) {
     case "install":
         console.log("install command called");
         break;
     case "list":
+        if (arg2 === 'available') {
+            getAvailableVersions().then((versions) => {
+                console.log(table(versions));
+                console.log("This is a partial list. For a complete list, visit https://nodejs.org/en/download/releases");
+            });
+        }
         console.log("list command called");
         break;
     case "use":
