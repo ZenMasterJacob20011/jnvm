d612dd6fe197fd8144e70225dd5a05b6a68ce6c1
Jacob Nichols <jacobnichols@form.io>
2025-06-03 00:24:47 -0500

    added on and off command

---
+++ src/commands/on.ts
@@ -0,0 +1,12 @@
+import {jnvmDirectory, symLinkPath} from "../runcommands";
+import fs from "fs";
+import {useVersion} from "./use";
+
+export async function on() {
+    const directory = await fs.promises.readdir(jnvmDirectory);
+    const nodeVersions = directory.filter(version => version.match(/^v\d{1,2}.\d{1,2}.\d{1,2}/));
+    const highestNodeVersion = nodeVersions.sort()[0];
+    await useVersion(highestNodeVersion, symLinkPath, jnvmDirectory);
+    console.log('jnvm enabled');
+    console.log(`Now using node ${highestNodeVersion} (64-bit)`);
+}
